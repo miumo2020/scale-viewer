17715fbf7cb0ff185dc328b35c6e2124da068e85
miumo <miumo2020@gmail.com>
2021-09-26 23:40:51 +0900

    Update: fix typo in tuning

--- src/components/ScaleViewer.tsx
+++ src/components/ScaleViewer.tsx
@@ -17,7 +17,7 @@ export const ScaleViewer = () => {
   const [state] = useState<State>({
     // C  Db D  Eb E  F  Gb G  Ab A Bb  B
     // 0  1  2  3  4  5  6  7  8  9 10 11
-    tuning: [3, 9, 2, 7, 11, 3],
+    tuning: [4, 9, 2, 7, 11, 4],
   });
 
   return (
