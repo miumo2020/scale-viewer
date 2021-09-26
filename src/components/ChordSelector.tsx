7a5d7873bded3a98d74e802fd70db5f826747a48
miumo <miumo2020@gmail.com>
2021-09-26 23:32:16 +0900

    Add: chord select box

--- src/components/ChordSelector.tsx
+++ src/components/ChordSelector.tsx
@@ -16,10 +16,41 @@ const Root = {
 } as const;
 type RootState = typeof Root[keyof typeof Root];
 
+const Chord: { [name: string]: string } = {
+  maj: "0,4,7",
+  m: "0,3,7",
+  "(b5)": "0,4,6",
+  dim: "0,3,6",
+  aug: "0,4,8",
+  sus4: "0,5,7",
+  sus2: "0,2,7",
+  "6": "0,4,7,9",
+  m6: "0,3,7,9",
+  "7": "0,4,7,10",
+  m7: "0,3,7,10",
+  M7: "0,4,7,11",
+  mM7: "0,3,7,11",
+  "7(b5)": "0,4,6,10",
+  "m7(b5)": "0,3,6,10",
+  "M7(b5)": "0,4,6,11",
+  "mM7(b5)": "0,3,6,11",
+  "7sus4": "0,5,7,10",
+  dim7: "0,3,6,9",
+  add9: "0,2,4,7",
+  "m(add9)": "0,2,3,7",
+  add4: "0,4,5,7",
+  aug7: "0,4,8,10",
+} as const;
+type ChordState = typeof Chord[keyof typeof Chord];
+
 export const ChordSelector = () => {
   const [root, setRoot] = useState<RootState>("C");
   const handleChange = (e: React.FormEvent<HTMLSelectElement>) => {
     setRoot(e.currentTarget.value as RootState);
+  };
+  const [chord, setChord] = useState<ChordState>();
+  const handleChangeChord = (e: React.FormEvent<HTMLSelectElement>) => {
+    setChord(e.currentTarget.value as ChordState);
   };
 
   const selectRoot = () => {
@@ -38,10 +69,30 @@ export const ChordSelector = () => {
     );
   };
 
+  const selectChord = () => {
+    const options = [];
+    for (let c in Chord) {
+      options.push(
+        <option value={c} key={c}>
+          {c}
+        </option>
+      );
+    }
+    return (
+      <>
+        <select onChange={(e) => handleChangeChord(e)}>{options}</select>
+      </>
+    );
+  };
+
   return (
     <>
       {selectRoot()}
-      <div>{root}</div>
+      {selectChord()}
+      <div>
+        {root}
+        {chord}
+      </div>
     </>
   );
 };
