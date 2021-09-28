41d7d5c6468c0c71bd02ebce4d111a93edf2692f
miumo <miumo2020@gmail.com>
2021-09-29 00:46:48 +0900

    Update: fix warning

--- src/components/FingerBoard.tsx
+++ src/components/FingerBoard.tsx
@@ -1,4 +1,4 @@
-// import React, {useState} from 'react';
+// import React, { useState } from "react";
 
 export const FingerBoard = () => {
   return (
@@ -27,49 +27,49 @@ export const FingerBoard = () => {
       <rect x="30" y="135" width="488" height="2" fill="#888"></rect>
       <rect x="30" y="165" width="488" height="2" fill="#888"></rect>
 
-      <text x="45" y="195" font-size="18" fill="#888" font-family="cursive">
+      <text x="45" y="195" fontSize="18" fill="#888" fontFamily="cursive">
         1
       </text>
-      <text x="77" y="195" font-size="18" fill="#888" font-family="cursive">
+      <text x="77" y="195" fontSize="18" fill="#888" fontFamily="cursive">
         2
       </text>
-      <text x="109" y="195" font-size="18" fill="#888" font-family="cursive">
+      <text x="109" y="195" fontSize="18" fill="#888" fontFamily="cursive">
         3
       </text>
-      <text x="141" y="195" font-size="18" fill="#888" font-family="cursive">
+      <text x="141" y="195" fontSize="18" fill="#888" fontFamily="cursive">
         4
       </text>
-      <text x="173" y="195" font-size="18" fill="#888" font-family="cursive">
+      <text x="173" y="195" fontSize="18" fill="#888" fontFamily="cursive">
         5
       </text>
-      <text x="205" y="195" font-size="18" fill="#888" font-family="cursive">
+      <text x="205" y="195" fontSize="18" fill="#888" fontFamily="cursive">
         6
       </text>
-      <text x="237" y="195" font-size="18" fill="#888" font-family="cursive">
+      <text x="237" y="195" fontSize="18" fill="#888" fontFamily="cursive">
         7
       </text>
-      <text x="269" y="195" font-size="18" fill="#888" font-family="cursive">
+      <text x="269" y="195" fontSize="18" fill="#888" fontFamily="cursive">
         8
       </text>
-      <text x="302" y="195" font-size="18" fill="#888" font-family="cursive">
+      <text x="302" y="195" fontSize="18" fill="#888" fontFamily="cursive">
         9
       </text>
-      <text x="330" y="195" font-size="18" fill="#888" font-family="cursive">
+      <text x="330" y="195" fontSize="18" fill="#888" fontFamily="cursive">
         10
       </text>
-      <text x="362" y="195" font-size="18" fill="#888" font-family="cursive">
+      <text x="362" y="195" fontSize="18" fill="#888" fontFamily="cursive">
         11
       </text>
-      <text x="394" y="195" font-size="18" fill="#888" font-family="cursive">
+      <text x="394" y="195" fontSize="18" fill="#888" fontFamily="cursive">
         12
       </text>
-      <text x="426" y="195" font-size="18" fill="#888" font-family="cursive">
+      <text x="426" y="195" fontSize="18" fill="#888" fontFamily="cursive">
         13
       </text>
-      <text x="458" y="195" font-size="18" fill="#888" font-family="cursive">
+      <text x="458" y="195" fontSize="18" fill="#888" fontFamily="cursive">
         14
       </text>
-      <text x="490" y="195" font-size="18" fill="#888" font-family="cursive">
+      <text x="490" y="195" fontSize="18" fill="#888" fontFamily="cursive">
         15
       </text>
     </svg>
