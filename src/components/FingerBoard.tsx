a1d5035f945a2947b07b91425c140e1fbbb95197
miumo <miumo2020@gmail.com>
2021-10-02 22:46:48 +0900

    Update: 横スクロールのためCSS追加

--- src/components/FingerBoard.tsx
+++ src/components/FingerBoard.tsx
@@ -110,77 +110,85 @@ export const FingerBoard: React.FC<FingerBoardProps> = (props) => {
   };
 
   return (
-    <svg width="518" height="200" viewBox="0 0 518 200">
-      <rect x="30" y="15" width="8" height="152" fill="#888"></rect>
-      <rect x="68" y="15" width="2" height=" 152" fill="#888"></rect>
-      <rect x="100" y="15" width="2" height=" 152" fill="#888"></rect>
-      <rect x="132" y="15" width="2" height=" 152" fill="#888"></rect>
-      <rect x="164" y="15" width="2" height=" 152" fill="#888"></rect>
-      <rect x="196" y="15" width="2" height=" 152" fill="#888"></rect>
-      <rect x="228" y="15" width="2" height=" 152" fill="#888"></rect>
-      <rect x="260" y="15" width="2" height=" 152" fill="#888"></rect>
-      <rect x="292" y="15" width="2" height=" 152" fill="#888"></rect>
-      <rect x="324" y="15" width="2" height=" 152" fill="#888"></rect>
-      <rect x="356" y="15" width="2" height=" 152" fill="#888"></rect>
-      <rect x="388" y="15" width="2" height=" 152" fill="#888"></rect>
-      <rect x="420" y="15" width="2" height=" 152" fill="#888"></rect>
-      <rect x="452" y="15" width="2" height=" 152" fill="#888"></rect>
-      <rect x="484" y="15" width="2" height=" 152" fill="#888"></rect>
-      <rect x="516" y="15" width="2" height=" 152" fill="#888"></rect>
+    <div style={FingerBoardStyle}>
+      <svg width="518" height="200" viewBox="0 0 518 200">
+        <rect x="30" y="15" width="8" height="152" fill="#888"></rect>
+        <rect x="68" y="15" width="2" height=" 152" fill="#888"></rect>
+        <rect x="100" y="15" width="2" height=" 152" fill="#888"></rect>
+        <rect x="132" y="15" width="2" height=" 152" fill="#888"></rect>
+        <rect x="164" y="15" width="2" height=" 152" fill="#888"></rect>
+        <rect x="196" y="15" width="2" height=" 152" fill="#888"></rect>
+        <rect x="228" y="15" width="2" height=" 152" fill="#888"></rect>
+        <rect x="260" y="15" width="2" height=" 152" fill="#888"></rect>
+        <rect x="292" y="15" width="2" height=" 152" fill="#888"></rect>
+        <rect x="324" y="15" width="2" height=" 152" fill="#888"></rect>
+        <rect x="356" y="15" width="2" height=" 152" fill="#888"></rect>
+        <rect x="388" y="15" width="2" height=" 152" fill="#888"></rect>
+        <rect x="420" y="15" width="2" height=" 152" fill="#888"></rect>
+        <rect x="452" y="15" width="2" height=" 152" fill="#888"></rect>
+        <rect x="484" y="15" width="2" height=" 152" fill="#888"></rect>
+        <rect x="516" y="15" width="2" height=" 152" fill="#888"></rect>
 
-      <rect x="30" y="15" width="488" height="2" fill="#888"></rect>
-      <rect x="30" y="45" width="488" height="2" fill="#888"></rect>
-      <rect x="30" y="75" width="488" height="2" fill="#888"></rect>
-      <rect x="30" y="105" width="488" height="2" fill="#888"></rect>
-      <rect x="30" y="135" width="488" height="2" fill="#888"></rect>
-      <rect x="30" y="165" width="488" height="2" fill="#888"></rect>
+        <rect x="30" y="15" width="488" height="2" fill="#888"></rect>
+        <rect x="30" y="45" width="488" height="2" fill="#888"></rect>
+        <rect x="30" y="75" width="488" height="2" fill="#888"></rect>
+        <rect x="30" y="105" width="488" height="2" fill="#888"></rect>
+        <rect x="30" y="135" width="488" height="2" fill="#888"></rect>
+        <rect x="30" y="165" width="488" height="2" fill="#888"></rect>
 
-      <text x="45" y="195" fontSize="18" fill="#888">
-        1
-      </text>
-      <text x="77" y="195" fontSize="18" fill="#888">
-        2
-      </text>
-      <text x="109" y="195" fontSize="18" fill="#888">
-        3
-      </text>
-      <text x="141" y="195" fontSize="18" fill="#888">
-        4
-      </text>
-      <text x="173" y="195" fontSize="18" fill="#888">
-        5
-      </text>
-      <text x="205" y="195" fontSize="18" fill="#888">
-        6
-      </text>
-      <text x="237" y="195" fontSize="18" fill="#888">
-        7
-      </text>
-      <text x="269" y="195" fontSize="18" fill="#888">
-        8
-      </text>
-      <text x="302" y="195" fontSize="18" fill="#888">
-        9
-      </text>
-      <text x="330" y="195" fontSize="18" fill="#888">
-        10
-      </text>
-      <text x="362" y="195" fontSize="18" fill="#888">
-        11
-      </text>
-      <text x="394" y="195" fontSize="18" fill="#888">
-        12
-      </text>
-      <text x="426" y="195" fontSize="18" fill="#888">
-        13
-      </text>
-      <text x="458" y="195" fontSize="18" fill="#888">
-        14
-      </text>
-      <text x="490" y="195" fontSize="18" fill="#888">
-        15
-      </text>
-      {markNotes()}
-    </svg>
+        <text x="45" y="195" fontSize="18" fill="#888">
+          1
+        </text>
+        <text x="77" y="195" fontSize="18" fill="#888">
+          2
+        </text>
+        <text x="109" y="195" fontSize="18" fill="#888">
+          3
+        </text>
+        <text x="141" y="195" fontSize="18" fill="#888">
+          4
+        </text>
+        <text x="173" y="195" fontSize="18" fill="#888">
+          5
+        </text>
+        <text x="205" y="195" fontSize="18" fill="#888">
+          6
+        </text>
+        <text x="237" y="195" fontSize="18" fill="#888">
+          7
+        </text>
+        <text x="269" y="195" fontSize="18" fill="#888">
+          8
+        </text>
+        <text x="302" y="195" fontSize="18" fill="#888">
+          9
+        </text>
+        <text x="330" y="195" fontSize="18" fill="#888">
+          10
+        </text>
+        <text x="362" y="195" fontSize="18" fill="#888">
+          11
+        </text>
+        <text x="394" y="195" fontSize="18" fill="#888">
+          12
+        </text>
+        <text x="426" y="195" fontSize="18" fill="#888">
+          13
+        </text>
+        <text x="458" y="195" fontSize="18" fill="#888">
+          14
+        </text>
+        <text x="490" y="195" fontSize="18" fill="#888">
+          15
+        </text>
+        {markNotes()}
+      </svg>
+    </div>
   );
+};
+
+const FingerBoardStyle: React.CSSProperties = {
+  overflow: "auto",
+  whiteSpace: "nowrap",
+  WebkitOverflowScrolling: "touch",
 };
