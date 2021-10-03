7a47132bf97d0004b37c5b3fb4be37ef051089cf
miumo <miumo2020@gmail.com>
2021-10-03 23:06:56 +0900

    Update: fix typo

--- src/components/Constants.ts
+++ src/components/Constants.ts
@@ -49,8 +49,8 @@ export const ScaleList: Scale[] = [
   { name: "Mixolydian", interval: [0, 2, 4, 5, 7, 9, 10] },
   { name: "Locrian", interval: [0, 1, 3, 5, 6, 8, 10] },
   { name: "Major Pentatonic", interval: [0, 2, 4, 7, 9] },
-  { name: "Minor Prntatonic", interval: [0, 3, 5, 7, 10] },
+  { name: "Minor Pentatonic", interval: [0, 3, 5, 7, 10] },
   { name: "Diminished", interval: [0, 2, 3, 5, 6, 8, 9, 11] },
-  { name: "Combination of Diminished", interval: [0, 1, 3, 4, 6, 7, 9, 10] },
+  // { name: "Combination of Diminished", interval: [0, 1, 3, 4, 6, 7, 9, 10] },
   { name: "Whole Tone", interval: [0, 2, 4, 6, 8, 10] },
 ];
